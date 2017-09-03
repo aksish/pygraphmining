0795ac2f21f4690b7a254a6cf573728008699d3c
Aashis <g2aashis@gmail.com>
2017-09-03 16:24:42 -0400

    Modified

--- utils/GTGraphToAux.py
+++ utils/GTGraphToAux.py
@@ -8,13 +8,15 @@ import numpy as np
 def to_auxiliary_matrix(gt_graph_file):
     data = np.loadtxt(gt_graph_file, dtype=int, skiprows=8, usecols=(1, 2))
     with open(gt_graph_file) as meta:
-        meta.readline();
-        meta_line = meta.readline();
+        print(meta.readline())
+        meta_line = meta.readline()
         nodes_count = int(meta_line.split(" ")[4])
+        print(meta.readline())
+        print(meta.readline())
+        print(meta.readline())
 
     auxiliary = np.zeros(shape=(nodes_count, nodes_count), dtype=int)
     for item in data:
-        print(item)
         row = item[0] - 1
         col = item[1] - 1
         auxiliary[row, col] = 1
@@ -23,7 +25,7 @@ def to_auxiliary_matrix(gt_graph_file):
 
 def main():
     os.chdir("D:\Linux VM\shared")
-    auxiliary_matrix = to_auxiliary_matrix("test1.gh")
+    auxiliary_matrix = to_auxiliary_matrix("assignment2_qsn1.gh")
 
     G = nx.from_numpy_matrix(auxiliary_matrix)
     nx.draw_random(G, with_labels=True)
