af83a4b95c8dd7e561a975a0dc4e95bb9095c388
Aashis <g2aashis@gmail.com>
2017-09-18 00:36:59 -0400

    PPR Done by inverse method also.

--- utils/page_rank.py
+++ utils/page_rank.py
@@ -53,8 +53,7 @@ def page_rank_by_power_iteration(decay=0.85):
             break
         page_ranks = new_page_ranks
 
-    print('-----------------------------')
-    print('Page Rank by power iteration:')
+    print('-----Page Rank by Power Iteration method-----')
     print(page_ranks.round(3))
 
 
@@ -87,11 +86,10 @@ def personalized_page_rank_by_power_iteration(decay=0.85, query_node=1):
             break
         page_ranks = new_page_ranks
 
-    print('-------------------------')
     print('Query Node: ' + str(query_node) + ' PPR:' + str(page_ranks.round(3)[query_node - 1]))
 
 
-def inverse_matrix_method(decay=0.85):
+def inverse_matrix_method_pr(decay=0.85):
     graph_aux_matrix = get_graph()
 
     # Calculate Transition probability matrix
@@ -117,29 +115,69 @@ def inverse_matrix_method(decay=0.85):
         if np.sum(col) == 0:
             m_matrix[:, i] = page_ranks
 
-
     page_ranks = np.linalg.inv(np.identity(n) - decay * m_matrix) * ((1 - decay) / n * np.ones((n, 1)))
-    print('----------------------------')
-    print('Page Rank by Inverse Method:')
+    print('------Page Rank by Inverse Matrix method------')
     print(page_ranks.round(3))
+
+
+def inverse_matrix_method_ppr(decay=0.85,query_node=1):
+    graph_aux_matrix = get_graph()
+
+    # Calculate Transition probability matrix
+    degree_count = np.matrix.sum(graph_aux_matrix, axis=1)
+    transition_matrix = graph_aux_matrix / degree_count
+
+    # Division by zero yields Nan value in place of zero . Below line code solves this problem
+    transition_matrix[np.isnan(transition_matrix)] = 0
+
+    # Total number of nodes = row length OR column length, ie graph_aux_matrix.shape[1] = column length
+    n = np.sum(graph_aux_matrix.shape[1])
+
+    # initialize with equal probability for a random visitor to visit each node. i.e. 1/N
+    page_ranks = np.full((n, 1), 1 / n)
+
+    m_matrix = transition_matrix.transpose()
+
+    q_vector = np.zeros((n, 1))
+    q_vector[query_node - 1] = 1
+
+    page_ranks = np.linalg.inv(np.identity(n) - decay * m_matrix) * (1 - decay) * q_vector
+    print('Query Node: ' + str(query_node) + ' PPR:' + str(page_ranks.round(3)[query_node - 1]))
 
 
 def main():
     # Just to disable some unnecessary warning
     np.seterr(divide='ignore', invalid='ignore')
+
     page_rank_by_power_iteration(decay=0.85)
-    personalized_page_rank_by_power_iteration(query_node=1)
-    personalized_page_rank_by_power_iteration(query_node=2)
-    personalized_page_rank_by_power_iteration(query_node=3)
-    personalized_page_rank_by_power_iteration(query_node=4)
-    personalized_page_rank_by_power_iteration(query_node=5)
-    personalized_page_rank_by_power_iteration(query_node=6)
-    personalized_page_rank_by_power_iteration(query_node=7)
-    personalized_page_rank_by_power_iteration(query_node=8)
-    personalized_page_rank_by_power_iteration(query_node=9)
-    personalized_page_rank_by_power_iteration(query_node=10)
-    personalized_page_rank_by_power_iteration(query_node=11)
-    inverse_matrix_method(decay=0.85)
+
+    inverse_matrix_method_pr(decay=0.85)
+
+    print('-------Personalized Page Rank by Power Iteration method-------')
+    personalized_page_rank_by_power_iteration(query_node=1, decay=0.85)
+    personalized_page_rank_by_power_iteration(query_node=2, decay=0.85)
+    personalized_page_rank_by_power_iteration(query_node=3, decay=0.85)
+    personalized_page_rank_by_power_iteration(query_node=4, decay=0.85)
+    personalized_page_rank_by_power_iteration(query_node=5, decay=0.85)
+    personalized_page_rank_by_power_iteration(query_node=6, decay=0.85)
+    personalized_page_rank_by_power_iteration(query_node=7, decay=0.85)
+    personalized_page_rank_by_power_iteration(query_node=8, decay=0.85)
+    personalized_page_rank_by_power_iteration(query_node=9, decay=0.85)
+    personalized_page_rank_by_power_iteration(query_node=10, decay=0.85)
+    personalized_page_rank_by_power_iteration(query_node=11, decay=0.85)
+
+    print('-------Personalized Page Rank by Matrix Inverse method-------')
+    inverse_matrix_method_ppr(query_node=1, decay=0.85)
+    inverse_matrix_method_ppr(query_node=2, decay=0.85)
+    inverse_matrix_method_ppr(query_node=3, decay=0.85)
+    inverse_matrix_method_ppr(query_node=4, decay=0.85)
+    inverse_matrix_method_ppr(query_node=5, decay=0.85)
+    inverse_matrix_method_ppr(query_node=6, decay=0.85)
+    inverse_matrix_method_ppr(query_node=7, decay=0.85)
+    inverse_matrix_method_ppr(query_node=8, decay=0.85)
+    inverse_matrix_method_ppr(query_node=9, decay=0.85)
+    inverse_matrix_method_ppr(query_node=10, decay=0.85)
+    inverse_matrix_method_ppr(query_node=11, decay=0.85)
 
 
 if __name__ == '__main__':
